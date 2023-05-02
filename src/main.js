9805b15e36103d6309ddbba19ba026abf336e633
Николай Стебунов <stebunoff@Nikolajs-MacBook-Pro.local>
2023-05-02 23:05:20 +0300

    Добавит форму редактирования

--- src/main.js
+++ src/main.js
@@ -1,11 +1,17 @@
-import { render } from './render';
+import { RenderPosition, render } from './render';
 import FilterView from './view/filters';
 import SortView from './view/sort';
 import TripEventView from './view/trip-event';
+import FormEditView from './view/form-edit';
 
 const filtersElement = document.querySelector('.trip-controls__filters');
 const tripEventsElement = document.querySelector('.trip-events');
 
-render(new FilterView(), filtersElement);
-render(new SortView(), tripEventsElement);
-render(new TripEventView(), tripEventsElement);
+render(new FormEditView(), tripEventsElement, RenderPosition.AFTERBEGIN);
+render(new FilterView(), filtersElement, RenderPosition.AFTERBEGIN);
+render(new SortView(), tripEventsElement, RenderPosition.AFTERBEGIN);
+
+for (let i = 0; i < 3; i++) {
+  render(new TripEventView(), tripEventsElement);
+}
+
