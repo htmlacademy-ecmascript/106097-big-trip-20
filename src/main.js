c4cc1ffeb1aec8b6b1c71b65bcd15cd1e2686c94
Николай Стебунов <stebunoff@Nikolajs-MacBook-Pro.local>
2023-05-02 22:43:22 +0300

    Добавляет шаблон точки маршрута

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 import { render } from './render';
 import FilterView from './view/filters';
 import SortView from './view/sort';
+import TripEventView from './view/trip-event';
 
 const filtersElement = document.querySelector('.trip-controls__filters');
 const tripEventsElement = document.querySelector('.trip-events');
 
 render(new FilterView(), filtersElement);
 render(new SortView(), tripEventsElement);
+render(new TripEventView(), tripEventsElement);
