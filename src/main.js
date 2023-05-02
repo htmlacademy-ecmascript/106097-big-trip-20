7a30ffa690f1734ddedbc0d3a9c040531c0f4c1b
Николай Стебунов <stebunoff@Nikolajs-MacBook-Pro.local>
2023-05-02 22:33:58 +0300

    Добавляет сортировку

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import { render } from './render';
 import FilterView from './view/filters';
+import SortView from './view/sort';
 
 const filtersElement = document.querySelector('.trip-controls__filters');
+const tripEventsElement = document.querySelector('.trip-events');
 
 render(new FilterView(), filtersElement);
+render(new SortView(), tripEventsElement);
