7a2ee65e94d35a8b9eb5dc81b41af6ad5bfa7588
Николай Стебунов <stebunoff@Nikolajs-MacBook-Pro.local>
2023-05-13 11:05:37 +0300

    Подключит модель

--- src/presenter/events-presenter.js
+++ src/presenter/events-presenter.js
@@ -7,16 +7,19 @@ import TripEventView from '../view/trip-event';
 export default class EventsPresenter {
   listComponent = new ListView();
 
-  constructor ({ boardContainer }) {
+  constructor ({ boardContainer, eventsModel }) {
     this.boardContainer = boardContainer;
+    this.eventsModel = eventsModel;
   }
 
   init () {
+    this.boardEvents = [...this.eventsModel.getEvents()];
+
     render(this.listComponent, this.boardContainer);
     render(new FormEditView(), this.listComponent.getElement());
     render(new SortView(), this.boardContainer, RenderPosition.AFTERBEGIN);
-    for (let i = 0; i < 3; i++) {
-      render(new TripEventView(), this.listComponent.getElement());
+    for (let i = 0; i < this.boardEvents.length; i++) {
+      render(new TripEventView({event: this.boardEvents[i]}), this.listComponent.getElement());
     }
   }
 }
