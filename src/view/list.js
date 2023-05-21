77dc7d7071105ac8ea9ef65b8e6e8af526ede25d
Николай Стебунов <stebunoff@Nikolajs-MacBook-Pro.local>
2023-05-21 18:14:44 +0300

    Исправит ошибку линтера

--- src/view/list.js
+++ src/view/list.js
@@ -1,14 +1,5 @@
 import {createElement} from '../render.js';
 
-const emptyListMessage = {
-  everything: 'Click New Event to create your first point',
-  past: 'There are no past events now',
-  present: 'There are no present events now',
-  future: 'There are no future events now',
-};
-
-const createEmptyListTemplate = (message) => `<p class="trip-events__msg">${message}</p>`;
-
 export default class ListView {
   constructor({list}) {
     this.list = list;
