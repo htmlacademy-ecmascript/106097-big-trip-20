ecc78f96214b8f58cb6a0e9046b23b6103cf4e02
Николай Стебунов <stebunoff@Nikolajs-MacBook-Pro.local>
2023-06-18 21:22:04 +0300

    Создаст класс для работы с API

---
+++ src/event-api-service.js
@@ -0,0 +1,26 @@
+import ApiService from './framework/api-service';
+
+const Method = {
+  GET: 'GET',
+  PUT: 'PUT'
+};
+
+export default class EventApiService extends ApiService {
+  get events() {
+    return this._load({url: 'points'})
+      .then(ApiService.parseResponse);
+  }
+
+  async updateEvent(event) {
+    const response = await this._load({
+      url: `points/${event.id}`,
+      method: Method.PUT,
+      body: JSON.stringify(event),
+      headers: new Headers({'Content-Type': 'application/json'}),
+    });
+
+    const parsedResponse = ApiService.parseResponse(response);
+
+    return parsedResponse;
+  }
+}
