11502e76e9c9f062d12b62e0dd492e1b78bff9ea
Николай Стебунов <stebunoff@Nikolajs-MacBook-Pro.local>
2023-06-18 11:46:06 +0300

    Ограничит цифрами допустимые символы ввода в поле с ценой

--- src/view/event-edit-view.js
+++ src/view/event-edit-view.js
@@ -97,7 +97,7 @@ function createFormEditTemplate (event, allOffers, destinations) {
           <span class="visually-hidden">Price</span>
           &euro;
         </label>
-        <input class="event__input  event__input--price" id="event-price-1" type="text" name="event-price" value="${cost}">
+        <input class="event__input  event__input--price" id="event-price-1" type="number" name="event-price" value="${cost}">
       </div>
 
       <button class="event__save-btn  btn  btn--blue" type="submit">Save</button>
